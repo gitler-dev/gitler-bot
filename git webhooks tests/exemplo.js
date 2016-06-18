42c3f4b0424c67054c0bdc397ba1f20cfea6c11c
Jorge <jorgesilva.me@gmail.com>
2016-06-18 21:54:09 +0100

    test

--- git webhooks tests/exemplo.js
+++ git webhooks tests/exemplo.js
@@ -15,15 +15,16 @@ handler.on('error', function (err) {
 
 handler.on('push', function (event) {
   //RECEIVED PUSH EVENT
+  var reposDir = "repos";
   var repoName = event.payload.repository.name;
 
   console.log('Received a push event for %s to %s', repoName, event.payload.ref);
 
   var fs = require('fs');
-  var simpleGit = require('simple-git')( "repos" );
+  var simpleGit = require('simple-git')( reposDir );
 
   //Check if repository is cloned
-  fs.access(path, fs.F_OK, function(err) {
+  fs.access(reposDir + "/" + repoName, fs.F_OK, function(err) {
       if (!err) {
             // repo exists - pull
             simpleGit.pull(event.payload.repository.html_url + ".git", localPath, function(){});
