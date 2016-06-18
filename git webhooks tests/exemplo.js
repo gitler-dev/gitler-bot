9ab1b49205458a20922b543874057e84aa319211
Jorge <jorgesilva.me@gmail.com>
2016-06-18 21:33:31 +0100

    fixes

--- git webhooks tests/exemplo.js
+++ git webhooks tests/exemplo.js
@@ -14,10 +14,32 @@ handler.on('error', function (err) {
 })
 
 handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref)
-})
+  //RECEIVED PUSH EVENT
+  var repoName = event.payload.repository.name
+
+  console.log('Received a push event for %s to %s', repoName, event.payload.ref);
+
+  var fs = require('fs');
+  var simpleGit = require('simple-git')( workingDirPath );
+
+
+  //Check if repository is cloned
+  fs.access(path, fs.F_OK, function(err) {
+      if (!err) {
+            // repo exists - pull
+            simpleGit.pull(function(err, update) {
+              if(update && update.summary.changes) {
+                 require('child_process').exec('npm restart');
+              }
+            });
+        } else {
+            // repo does not exist - clone
+            simpleGit.clone(repoPath, localPath, handlerFn)
+        }
+    });
+  
+  
+});
 
 handler.on('issues', function (event) {
   console.log('Received an issue event for %s action=%s: #%d %s',
