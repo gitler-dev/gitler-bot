2457aac75b67c6c1221703238a5cdc1684e98b10
João Anes <joao.anes@gmail.com>
2016-06-19 01:45:48 +0100

    Added bot file to this repo

--- gitcli.js
+++ gitcli.js
@@ -1,15 +1,33 @@
-var simpleGit = require('simple-git')( __dirname ).filesChangedHead(function(err, diff) {
-		diff.files.forEach(function(file){
-        	console.log(file.file)
-        })
-      });
+var simpleGit = require('simple-git')
+var _ = require('underscore')
+var fs = require('fs')
+var Promise = require('bluebird')
+
+simpleGit.filesChanged
+
+function getFilesChangedFromGit(repoDir, ref1, ref2) {
+
+	return new Promise(function(res, rej){
+		simpleGit(repoDir).filesChanged(ref1, ref2, function(err, diff){
+			if (err) return null;
+			
+			if (!diff) rej("no")
+			var filesMap = _.map(diff.files, function(file){
+				return {file: file.file, body: fs.readFileSync(repoDir + '/' + file.file, "utf-8")}
+	        })
+			
+			res(filesMap)
+		})
+
+	})
+		
+}
 
 
-hebhook.onPr(function(pr,ref){
-	git.filesChanged(ref,'master',function(err,diff)
-	{
-		diff.files.forEach(function(file){
-        	console.log(file.file)
-        })
-	});
-});
+
+
+getFilesChangedFromGit(__dirname, "HEAD", "HEAD~10").then(function(files){
+	console.log(files + "ngr")
+})
+
+exports.getFilesChangedFromGit = getFilesChangedFromGit
